19adda4865832fa37b8eb2e32cfd4ea978801ab6
zuodimiaoyun <zuodimiaoyun@gmail.com>
2017-09-10 11:29:32 +0800

    change package to clean package, change port to 8080, add run.log to .gitignore

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn package
-java -jar target/todo-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod
+mvn clean package
+nohup java -jar target/todo-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod 2>&1 > run.log &
